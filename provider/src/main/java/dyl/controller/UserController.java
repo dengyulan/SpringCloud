c94e36cdc914cba6ae1d4b549bf165567426ad00
dengyulan <852238252@qq.com>
2023-04-02 19:36:16 +0800

    测试git提交

--- provider/src/main/java/dyl/controller/UserController.java
+++ provider/src/main/java/dyl/controller/UserController.java
@@ -8,12 +8,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserController {
 
-    @PostMapping("/query/user")
-    public User queryUser(@RequestBody JSONObject jsonObject){
-        System.out.println(jsonObject);
-        return new User("dyl", 18);
-    }
-
     /**
      * 该接口用于测试openfeign中post的form-data接口
      */
